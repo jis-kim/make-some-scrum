11461d7f3a70993eb5545828d5813ee0a7b5f316
Jisu <hellojs242@gmail.com>
2024-10-02 15:13:51 +0900

    :sparkles: feat: 다음 달로 넘어갈 경우 다음달 값도 가져오는 api 요청 추가

--- holiday.js
+++ holiday.js
@@ -1,18 +1,53 @@
 import { XMLParser } from 'fast-xml-parser';
 
+const parser = new XMLParser({
+  ignoreAttributes: true,
+  parseTagValue: false,
+});
+
+const url =
+  'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo';
+
+const getNextMonthHoliday = async (date, items) => {
+  const key = process.env.HOLIDAY_API_KEY;
+  if (date.getDate() < 22) {
+    return;
+  }
+
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+
+  if (month === 12) {
+    year += 1;
+    month = 1;
+  } else {
+    month += 1;
+  }
+
+  const api = `${url}?solYear=${year}&solMonth=${month}&ServiceKey=${key}`;
+  const response = await fetch(api);
+  const xml = await response.text();
+  const array = parser.parse(xml).response.body?.items?.item;
+  if (array !== undefined && array.length > 0) {
+    items.push(array);
+  }
+};
+
 export const getHoliday = async () => {
   const date = new Date();
+  const key = process.env.HOLIDAY_API_KEY;
 
   // UTC 시간에서 9시간 더해서 KST로 변환
-  date.setHours(date.getUTCHours() + 9); // UTC 기준으로 9시간 더하기 (KST = UTC + 9)
+  // 갑자기.. 난 15일에 할건데 굳이 이걸.............해야 하나? 하는 생가ㄱ이..
+  //date.setHours(date.getUTCHours() + 9); // UTC 기준으로 9시간 더하기 (KST = UTC + 9)
 
-  const key = process.env.HOLIDAY_API_KEY;
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
 
-  const api = `http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?solYear=${year}&solMonth=${month}&ServiceKey=${key}`;
+  // 만약에 오늘 날짜가 22일 이상이면 다음 달의 공휴일도 가져온다.
+  // 22, 23, 24, 25토, 26일, 27월, 28화, 29수, 30목, 31금, 30일이면 목요일일수도..
 
-  console.log(api);
+  const api = `${url}?solYear=${year}&solMonth=${month}&ServiceKey=${key}`;
 
   // API 호출
   const response = await fetch(api);
@@ -21,10 +56,6 @@ export const getHoliday = async () => {
   console.log(xml);
 
   // XML 파싱
-  const parser = new XMLParser({
-    ignoreAttributes: true,
-    parseTagValue: false,
-  });
   /**
  * {
   '?xml': '',
@@ -49,9 +80,17 @@ export const getHoliday = async () => {
 }
  */
 
-  return parser.parse(xml).response.body.items.item.map((item) => {
-    return {
-      date: item.locdate,
-    };
+  const items = [];
+  const array = parser.parse(xml).response.body?.items?.item;
+  if (array !== undefined && array.length > 0) {
+    items.push(...array);
+  }
+  await getNextMonthHoliday(date, items);
+
+  return items.map((item) => {
+    const date = item.locdate;
+    return new Date(
+      `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6, 8)}`
+    );
   });
 };
--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+/**
+ * 주 1회 수요일에 실행되는 스크립트.
+ * 만약 공휴일이면 스크럼을 생성하지 않음.
+ */
 import { getHoliday } from './holiday.js';
+import { createNotionPage } from './notion.js';
 
 import dotenv from 'dotenv';
 
@@ -7,7 +12,7 @@ dotenv.config();
 // 공휴일 API 받아오기
 const holidays = await getHoliday();
 console.log(holidays);
-process.exit(0);
+await createNotionPage(holidays);
 
 // holiday 제외하고 월화수목금 10:30에 스크럼 생성
 // 현재 날짜가 공휴일이면 스크럼 생성하지 않음
